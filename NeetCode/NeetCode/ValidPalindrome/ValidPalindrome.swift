c23ceddfa2e110f945962eeef0997dcfc4df8435
Taichi <taichi.nishimura0206@gmail.com>
2026-06-19 07:25:00 +0900

    2日目

--- NeetCode/NeetCode/ValidPalindrome/ValidPalindrome.swift
+++ NeetCode/NeetCode/ValidPalindrome/ValidPalindrome.swift
@@ -30,5 +30,29 @@ import Foundation
 class Solutio3 {
     func isPalindrome(_ s: String) -> Bool {
         
+        let chars = Array(s.lowercased())
+        
+        var left: Int = 0
+        var right: Int = s.count - 1
+          
+        while left < right {
+            if !chars[left].isLetter && !chars[left].isNumber {
+                left += 1
+                continue
+            }
+            
+            if !chars[right].isLetter && !chars[right].isNumber {
+                right += 1
+                continue
+            }
+            
+            if chars[left].lowercased() != chars[right].lowercased() {
+                return false
+            }
+            
+            left += 1
+            right -= 1
+        }
+        return true
     }
 }
