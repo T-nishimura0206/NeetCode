2b6750b47e0f61242a149608b010f0057ae69327
Taichi <taichi.nishimura0206@gmail.com>
2026-06-21 12:51:44 +0900

    4日目

---
+++ NeetCode/NeetCode/ValidParentheses.swift
@@ -0,0 +1,32 @@
+//
+//  ValidParentheses.swift
+//  NeetCode
+//
+//  Created by 西村太一 on 2026/06/21.
+//
+
+import Foundation
+
+class Solution5 {
+    func isValid(_ s: String) -> Bool {
+        
+        var stack: [Character] = []
+        var pairs: [Character:Character] = ["(":")", "{":"}", "[":"]"]
+       
+        for c in s {
+            if c == "(" || c == "[" || c == "{" {
+                // stackに追加
+                stack.append(c)
+            } else {
+                if stack.isEmpty { // stackが空ならfalse（開き括弧があるはずだから）
+                    return false
+                } else if let last = stack.last, pairs[last] != c { // 最後の要素と比較
+                    return false
+                } else {
+                    stack.removeLast()// 一致ならremoveLast()
+                }
+            }
+        }
+        return stack.isEmpty ? true : false
+    }
+}
